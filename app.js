2e0ba8dbb21b8d99d4021bd1532e87207072a6c0
karmanya17 <53150531+karmanya17@users.noreply.github.com>
2021-06-05 23:53:21 +0530

    Server

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const https=require("https");
+const bodyParser=require("body-parser");
+const path=require("path");
+const fs=require("fs");
+const app=express();
+const route=require("./routes/sendroutes");
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use("/",express.static(path.join(__dirname,"public")));
+
+app.use("/message",route);
+
+// const sslServer= https.createServer(
+//   {
+//   key:fs.readFileSync(path.join(__dirname,'certificate','key.pem')),
+//   certificate:fs.readFileSync(path.join(__dirname,"certificate","certificate.pem")),
+//
+// },
+// app
+// )
+// sslServer.listen(3000,function(){
+//   console.log("Secure Server");
+// })
+
+app.listen("3000",function(req,res){
+  console.log("server is up at 3000");
+})
